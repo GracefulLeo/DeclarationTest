663548cf9fd621968eff3380488dee5855ac2e6a
GracefulLeo <29eb1f1ca>
2018-07-11 12:19:23 +0300

    BottomNavigation view added with some icons for it. Related xml is changed

--- app/src/main/java/com/example/adventurer/declarationtest/ViewActivity.java
+++ app/src/main/java/com/example/adventurer/declarationtest/ViewActivity.java
@@ -1,14 +1,51 @@
 package com.example.adventurer.declarationtest;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
 
-public class ViewActivity extends AppCompatActivity{
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+public class ViewActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    //TAGS
+    private static final String TAG = "View Activity";
+
+    //Widgets
+    private BottomNavigationViewEx bottomNavigationViewEx;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_main);
+        bottomNavigationViewEx = findViewById(R.id.bottom_nav_view);
+
+        initBottomNavigationView();
+    }
+
+    private void initBottomNavigationView () {
+        Log.d(TAG, "initBottomNavigationView: initizialization of bottom navigation view.");
+        bottomNavigationViewEx.enableAnimation(true);
+        bottomNavigationViewEx.setOnNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.bottom_nav_home:
+                Log.d(TAG, "onNavigationItemSelected: home..");
+                item.setChecked(true);
+                break;
+            case R.id.bottom_nav_favourites:
+                Log.d(TAG, "onNavigationItemSelected: favourites...");
+                item.setChecked(true);
+                break;
+        }
+    return false;
     }
 }
